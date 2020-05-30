4193f15d027ad1b0e360268b1a57fe153bcb926a
NedobytkoR <nedobytko.r@ajax.systems>
2020-05-30 14:04:30 +0300

    add validate fields

--- internal/app/model/comment.go
+++ internal/app/model/comment.go
@@ -1,10 +1,25 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 // Comment ..
 type Comment struct {
-	ID int 
-	Author string
-	Content string
+	ID          int
+	Author      string
+	Content     string
 	CreatedTime string
-	Post *Post
+	Post        *Post
+}
+
+// Validate ...
+func (c *Comment) Validate() error {
+	return validation.ValidateStruct(
+		c,
+		validation.Field(&c.Author, validation.Required, validation.Length(6, 255)),
+		validation.Field(&c.Content, validation.Required),
+		validation.Field(&c.Post, validation.Required, is.Digit),
+	)
 }
--- internal/app/model/post.go
+++ internal/app/model/post.go
@@ -1,12 +1,26 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 // Post model
 type Post struct {
-	ID int
-	Title string
-	Link string
-	AuthorName string
-	CreatedTime string
+	ID           int
+	Title        string
+	Link         string
+	AuthorName   string
+	CreatedTime  string
 	AmountUpvote int
 }
 
+// Validate fields
+func (p *Post) Validate() error {
+	return validation.ValidateStruct(
+		p,
+		validation.Field(&p.Title, validation.Required, validation.Length(6, 255)),
+		validation.Field(&p.Link, validation.Required, is.URL),
+		validation.Field(&p.AuthorName, validation.Required),
+	)
+}
